c7490b9722a760b75a28c1351568d51dd9d07a8c
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-21 12:47:47 +0300

    added intilizers in MContext constructor

--- source/Axum/MContext.cpp
+++ source/Axum/MContext.cpp
@@ -12,5 +12,13 @@
  * Current context of the application.
  */
 MContext::MContext(){
-
+    //Order of intilization does not matter as the constructors does not do any thing.
+    this->AddonMan = AddonManager();
+    this->BakeryMan = BakeryManager();
+    this->EditorMan = EditorManager();
+    this->PackageMan = PackageManager();
+    this->PrefMan = PreferenceManager();
+    this->RenderMan = RenderManager();
+    this->UndoMan = UndoManager();
+    this->WinMan = WindowManager();
 }
