caf35056560a40e803fbc0e4575fc6b3901009cb
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-21 12:37:37 +0300

    reordered function calls

--- source/Axum/UI/SplashWindow.cpp
+++ source/Axum/UI/SplashWindow.cpp
@@ -8,9 +8,9 @@ SplashWindow::SplashWindow(int width,int height){
     this->set_type_hint(Gdk::WINDOW_TYPE_HINT_SPLASHSCREEN);
 }
 SplashWindow::SplashWindow(){
+    this->set_size_request(500,500);
     this->set_decorated(false);
+    this->set_resizable(false);
     this->set_position(Gtk::WIN_POS_CENTER_ALWAYS);
     this->set_type_hint(Gdk::WINDOW_TYPE_HINT_SPLASHSCREEN);
-    this->set_resizable(false);
-    this->set_size_request(500,500);
 }
