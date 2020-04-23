ecbcc91a305cb22f871e976c2292840eab88a758
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-23 22:16:41 +0300

    setup undo-test

---
+++ source/tests/undo-test/undo_test.cpp
@@ -0,0 +1,16 @@
+
+#include <gmock/gmock.h>
+
+
+#include <UndoManager.h>
+#include "PreferenceManager.h"
+
+class undo_test: public testing::Test
+{
+public:
+};
+
+TEST_F(undo_test, CheckSomeResults)
+{
+    // ...
+}
