d9844c821830eaa503ea53d3312bf046a61ae32c
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-24 12:08:50 +0300

    add serilization

--- source/Axum/Utils/Serialization/GUIInfo.h
+++ source/Axum/Utils/Serialization/GUIInfo.h
@@ -7,11 +7,33 @@
 #ifndef _GUIINFO_H
 #define _GUIINFO_H
 
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/version.hpp"
+#include "boost/serialization/split_member.hpp"
 class GUIInfo {
 public: 
 	int X = 0;
 	int Y = 0;
 	bool isHidden = false;
+//serilization
+private:
+friend class boost::serialization::access;
+template<class Archive>
+void save(Archive &ar, const unsigned int version) const
+{
+    ar &X;
+	ar &Y;
+	ar &isHidden;
+}
+template<class Archive>
+void load(Archive &ar, const unsigned int version){
+    ar &X;
+	ar &Y;
+	ar &isHidden;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+public:
+	GUIInfo();
 };
-
+BOOST_CLASS_VERSION(GUIInfo, 1)
 #endif //_GUIINFO_H
