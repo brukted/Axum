5729a575a78a283a0fa236432371b7b2da309a2c
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-25 12:43:01 +0300

    make serilizable

--- source/Axum/Package/Package.h
+++ source/Axum/Package/Package.h
@@ -10,11 +10,15 @@
 #include<vector>
 #include"../NodeGraph/Graph.h"
 #include"../Resources/Resource.h"
-
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/vector.hpp"
+#include "boost/serialization/version.hpp"
+#include "boost/serialization/split_member.hpp"
 class Package {
+friend class boost::serialization::access;
 public: 
 	std::vector<Resource> Resources;
-	
+	unsigned int uid;
 /**
  * @param graph
  */
@@ -29,6 +33,21 @@ void DeleteGraph(unsigned int UUID);
  * @param UUID
  */
 Resource* GetResource(unsigned int UUID);
-};
+private: 
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &uid;
+    ar &Resources;
+}
 
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &uid;
+    ar &Resources;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(Package,1)
 #endif //_PACKAGE_H
