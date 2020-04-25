e5747c6964d5c76083bc45b1f5792fd47c1f12a9
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-25 12:26:14 +0300

    make serilizable

--- source/Axum/NodeGraph/Graph.h
+++ source/Axum/NodeGraph/Graph.h
@@ -11,8 +11,12 @@
 #include "../Resources/Resource.h"
 #include "Node.h"
 #include "../Parameter/GraphParam.h"
-
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/split_member.hpp"
+#include "boost/serialization/version.hpp"
+#include "boost/serialization/vector.hpp"
 class Graph: public Resource {
+friend class boost::serialization::access;
 public: 
 	/**
 	 * List of parameters that nodes in the node tree can refer to.
@@ -40,6 +44,23 @@ void SetupCache();
 void ClearCache();
 	
 void DeleteCache();
-};
+private: 
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &uid;
+    ar &mParms;
+	ar &mNodes;
+}
 
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &uid;
+    ar &mParms;
+	ar &mNodes;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(Graph,1)
 #endif //_GRAPH_H
