9de91bd1338b0d966860b6f1a6d70edc68805dcf
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-27 12:24:19 +0300

    make serilizable

--- source/Axum/Resources/Folder.h
+++ source/Axum/Resources/Folder.h
@@ -9,11 +9,31 @@
 
 #include<vector>
 #include "Resource.h"
-
-
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/version.hpp"
+#include "boost/serialization/vector.hpp"
+#include "boost/serialization/split_free.hpp"
+#include "boost/serialization/base_object.hpp"
 class Folder: public Resource {
+friend class boost::serialization::access;
 public: 
 	std::vector<Resource> mResources;
+private: 
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &boost::serialization::base_object<Resource>(*this);
+	ar &mResources;
+}
+
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &boost::serialization::base_object<Resource>(*this);
+	ar &mResources;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
+BOOST_CLASS_VERSION(Folder,1)
 
 #endif //_FOLDER_H
