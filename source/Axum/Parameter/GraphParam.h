a07a96edc105e3cd1f90962dc36bec7a6b2b1998
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-29 12:12:25 +0300

    make serilizable

--- source/Axum/Parameter/GraphParam.h
+++ source/Axum/Parameter/GraphParam.h
@@ -9,9 +9,28 @@
 
 #include "Param.h"
 #include "ParamCollection.h"
-
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/split_member.hpp"
+#include "boost/serialization/version.hpp"
 
 class GraphParam:public ParamCollection {
+
+friend class boost::serialization::access;
+
+private:
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &boost::serialization::base_object<ParamCollection>(*this);
+}
+
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &boost::serialization::base_object<ParamCollection>(*this);
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
+BOOST_CLASS_VERSION(GraphParam,1)
 
 #endif //_GRAPHPARAM_H
