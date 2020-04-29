ada294b6c8d0415c583ec7c6b22c59b1cb2ea370
Bruk Tedla <biruk.tedla93@gmail.com>
2020-04-29 12:08:12 +0300

    make serializable

--- source/Axum/Parameter/ParamCollection.h
+++ source/Axum/Parameter/ParamCollection.h
@@ -9,9 +9,13 @@
 
 #include <vector>
 #include "Param.h"
-
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/split_member.hpp"
+#include "boost/serialization/vector.hpp"
+#include "boost/serialization/version.hpp"
 
 class ParamCollection: public Param {
+friend class boost::serialization::access;
 public: 
 	std::vector<Param> Params;
 	
@@ -26,6 +30,21 @@ Param* GetParameter(unsigned int uid);
  * @param Parameter
  */
 void AddParameter(Param Parameter);
-};
+private:
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &boost::serialization::base_object<Param>(*this);
+	ar &Params;
+}
 
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &boost::serialization::base_object<Param>(*this);
+	ar &Params;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(ParamCollection,1)
 #endif //_PARAMCOLLECTION_H
