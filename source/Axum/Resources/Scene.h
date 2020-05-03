6c31d91e561790a16624c5965358805c096665e9
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-03 15:00:11 +0300

    make serialiazable

--- source/Axum/Resources/Scene.h
+++ source/Axum/Resources/Scene.h
@@ -9,19 +9,65 @@
 
 #include<vector>
 #include "Resource.h"
+#include "boost/serialization/access.hpp"
+#include "boost/serialization/array.hpp"
+#include "boost/serialization/base_object.hpp"
+#include "boost/serialization/split_member.hpp"
+#include "boost/serialization/vector.hpp"
+#include "boost/serialization/version.hpp"
 
+class Mesh 
+{
+public: 
+std::vector<float[3]> Vertices;
+std::vector<float[3]> Normals;
+std::vector<unsigned int> Indices;
+std::vector<Mesh> SubMeshs;
+int MaterialId;
+
+private: 
+bool NeedUpdate = true;
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &Vertices;
+	ar &Normals;
+	ar &Indices;
+	ar &SubMeshs
+	ar &MaterialId;
+}
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &Vertices;
+	ar &Normals;
+	ar &Indices;
+	ar &SubMeshs
+	ar &MaterialId;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(Mesh,1)
 
 class Scene: public Resource {
+friend class boost::serialization::access;
 public:
-	class Mesh {
-	public: 
-		std::vector<float[3]> Vertices;
-		std::vector<float[3]> Normals;
-		std::vector<unsigned int> Indices;
-		std::vector<Mesh> SubMeshs;
-		int MaterialId;
-	};
 	std::vector<Mesh> meshs;
-};
+private:
+template<class Archive>
+void save(Archive & ar, const unsigned int version) const
+{
+	ar &boost::serialization::base_object<Resource>(*this);
+	ar &meshs;
+}
 
+template<class Archive>
+void load(Archive & ar, const unsigned int version)
+{
+	ar &boost::serialization::base_object<Resource>(*this);
+	ar &meshs;
+}
+BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(Scene,1)
 #endif //_SCENE_H
