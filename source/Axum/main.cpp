f22ea73fe86c33a921b5566f260a3fb79bb5ebd5
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-11 18:38:33 +0300

    added log

--- source/Axum/main.cpp
+++ source/Axum/main.cpp
@@ -5,5 +5,6 @@ int main(int argc, char *argv[])
 {
     Log::getInstance().Startup();
     auto application = MApplication::create();
+    AX_LOG_EDITOR_INFO("Application created.")
     return application->run(argc, argv);
 }
