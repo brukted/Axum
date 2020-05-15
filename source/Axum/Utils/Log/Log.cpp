18600ed934d3187b58875c79798407ec0dae8a20
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-15 16:46:31 +0300

    bug fix

--- source/Axum/Utils/Log/Log.cpp
+++ source/Axum/Utils/Log/Log.cpp
@@ -3,14 +3,14 @@
 void Log::Startup()
 {
     spdlog::set_pattern("%^ [%T] %n: %v%$");
-    Log::addonLogger = std::make_shared<spdlog::logger>("Addon", Log::sink);
-    Log::coreLogger = std::make_shared<spdlog::logger>("Core", Log::sink);
-    Log::editorLogger = std::make_shared<spdlog::logger>("Editor", Log::sink);
-    Log::renderLogger = std::make_shared<spdlog::logger>("Render", Log::sink);
 //Debug build
 #ifndef NDEBUG
     Log::sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
     Log::sink->set_level(spdlog::level::trace);
+    Log::addonLogger = std::make_shared<spdlog::logger>("Addon", Log::sink);
+    Log::coreLogger = std::make_shared<spdlog::logger>("Core", Log::sink);
+    Log::editorLogger = std::make_shared<spdlog::logger>("Editor", Log::sink);
+    Log::renderLogger = std::make_shared<spdlog::logger>("Render", Log::sink);
     Log::addonLogger->set_level(spdlog::level::trace);
     Log::coreLogger->set_level(spdlog::level::trace);
     Log::editorLogger->set_level(spdlog::level::trace);
@@ -20,6 +20,10 @@ void Log::Startup()
     ALog::sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt");
     spdlog::set_level(spdlog::level::info);
     Log::sink->set_level(spdlog::level::info);
+    Log::addonLogger = std::make_shared<spdlog::logger>("Addon", Log::sink);
+    Log::coreLogger = std::make_shared<spdlog::logger>("Core", Log::sink);
+    Log::editorLogger = std::make_shared<spdlog::logger>("Editor", Log::sink);
+    Log::renderLogger = std::make_shared<spdlog::logger>("Render", Log::sink);
     Log::addonLogger->set_level(spdlog::level::info);
     Log::coreLogger->set_level(spdlog::level::info);
     Log::editorLogger->set_level(spdlog::level::info);
