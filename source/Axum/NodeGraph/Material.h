de60eac29f61da1e6b4d7ca79a519d50678d5fd4
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-19 14:59:40 +0300

    refactored enums

---
+++ source/Axum/NodeGraph/Material.h
@@ -0,0 +1,17 @@
+enum class Channel
+{
+    RGBA,
+    RGB,
+    R,
+    G,
+    B,
+    A
+};
+
+enum ImageFormat
+{
+    EIGHT_BIT,
+    SIXTEEN_BIT,
+    SIXTEEN_BIT_FLOAT,
+    THIRTY_TWO_BIT_FLOAT
+};
