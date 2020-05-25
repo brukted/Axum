8e9719254e5bf0825d7cd2d98720a2d3e81409b0
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-25 16:58:33 +0300

    bug fixes and better logging

--- source/Axum/Managers/PackageManager.cpp
+++ source/Axum/Managers/PackageManager.cpp
@@ -18,29 +18,33 @@
  */
 void PackageManager::LoadPackage(std::string &Path)
 {
-    auto p = boost::filesystem::path(Path);
-    if (boost::filesystem::exists(p))
+    auto start = std::chrono::steady_clock::now();
+    AX_LOG_EDITOR_INFO(fmt::format("Opening a package at %s", Path))
+    std::ifstream ifs(Path);
+    if (ifs.good())
     {
-        std::ofstream ofs(Path);
-        boost::archive::text_oarchive oa(ofs);
+        boost::archive::text_iarchive ia(ifs);
         mPackages.push_back(Package());
-        oa << mPackages.back();
+        ia >> mPackages.back();
         mPackages.back().setPath(Path.c_str());
     }
     else
     {
-        AX_LOG_EDITOR_WARN("Tried to open a file that doesn't exist.")
-        std::__throw_runtime_error("File doesn't exist.");
+        AX_LOG_EDITOR_WARN("Input file stream is bad maybe the file doesn't exist.")
+        std::__throw_runtime_error("Bad input file stream");
+        return;
     }
+    auto end = std::chrono::steady_clock::now();
+    AX_LOG_EDITOR_INFO(fmt::format("Loaded package %s in : %i milliseconds", mPackages.back().name.GetValue(), std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()))
 }
 
 void PackageManager::LoadPackage(const Glib::RefPtr<Gio::File> &file)
 {
-    if (file->query_exists())
+    auto start = std::chrono::steady_clock::now();
+    AX_LOG_EDITOR_INFO(fmt::format("Opening a package at %s", file->get_path()))
+    std::ifstream ifs(file->get_path().c_str());
+    if (ifs.good())
     {
-        // open the archive
-        std::ifstream ifs(file->get_path().c_str());
-        assert(ifs.good());
         boost::archive::text_iarchive ia(ifs);
         mPackages.push_back(Package());
         ia >> mPackages.back();
@@ -48,16 +52,22 @@ void PackageManager::LoadPackage(const Glib::RefPtr<Gio::File> &file)
     }
     else
     {
-        AX_LOG_EDITOR_WARN("Tried to open file that doesn't exist.")
-        std::__throw_runtime_error("File doesn't exist.");
+        AX_LOG_EDITOR_WARN("Input file stream is bad maybe the file doesn't exist.")
+        std::__throw_runtime_error("Bad input file stream");
+        return;
     }
+    auto end = std::chrono::steady_clock::now();
+    AX_LOG_EDITOR_INFO(fmt::format("Loaded package %s in : %i milliseconds", mPackages.back().name.GetValue(), std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()))
 }
 
 void PackageManager::SavePackage(Package &pkg)
 {
+    auto start = std::chrono::steady_clock::now();
     std::ofstream ofs(pkg.mPath);
     boost::archive::text_oarchive oa(ofs);
     oa << pkg;
+    auto end = std::chrono::steady_clock::now();
+    AX_LOG_EDITOR_INFO(fmt::format("Saved package %s in : %i milliseconds", mPackages.back().name.GetValue(), std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()))
 }
 
 void PackageManager::Startup() noexcept
