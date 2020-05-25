4473585075a4b86f45ca5d7d460ce4deda075358
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-25 13:56:26 +0300

    made functional

--- source/Axum/NodeGraph/MaterialGraph.cpp
+++ source/Axum/NodeGraph/MaterialGraph.cpp
@@ -9,16 +9,37 @@
  * MaterialGraph implementation
  */
 
-MaterialGraph::MaterialGraph()
+MaterialGraph::MaterialGraph(unsigned int _uid) : Graph(_uid)
 {
+    size.AddParameter(&width);
+    size.AddParameter(&height);
+    defaultParams.AddParameter(&size);
+    defaultParams.AddParameter(&imageFormat);
+    defaultParams.AddParameter(&seed);
+    mParams.AddParameter(&defaultParams);
 }
 
 void MaterialGraph::SetupCache()
 {
+    for (auto &n : mNodes)
+    {
+        static_cast<MaterialNode &>(n).SetupCache();
+    }
 }
-void MaterialGraph::ClearCache()
-{
-}
+
 void MaterialGraph::DeleteCache()
 {
+    for (auto &n : mNodes)
+    {
+        static_cast<MaterialNode &>(n).DeleteCache();
+    }
+}
+
+void MaterialGraph::Process()
+{
+    auto nodes = this->transverse();
+    for (auto n : nodes)
+    {
+        static_cast<MaterialNode *>(n)->Excute();
+    }
 }
--- source/Axum/NodeGraph/MaterialGraph.h
+++ source/Axum/NodeGraph/MaterialGraph.h
@@ -7,14 +7,51 @@
 #define _MATERIALGRAPH_H
 
 #include "Graph.h"
+#include "Nodes/MaterialNode.h"
+#include "Material.h"
+#include "../../Parameter/EnumParam.h"
+#include "../../Parameter/Float2Param.h"
+#include "../../Parameter/IntegerParam.h"
+#include "../../Parameter/Integer2Param.h"
 
 class MaterialGraph : public Graph
 {
-public:
-    MaterialGraph();
-    void SetupCache();
-    void ClearCache();
-    void DeleteCache();
-};
+    friend class boost::serialization::access;
 
+public:
+    ParamCollection defaultParams{mParams.GenerateUid(), "Default Parameters"};
+    ParamCollection size{defaultParams.GenerateUid(), "Size"};
+    EnumParam width{size.GenerateUid(), "Width", outputSizes, 1024};
+    EnumParam height{size.GenerateUid(), "Height", outputSizes, 1024};
+    EnumParam imageFormat{defaultParams.GenerateUid(), "Image format", imageFormats, ImageFormat::EIGHT_BIT};
+    IntegerParam seed{defaultParams.GenerateUid(), "Random seed", 0};
+    ParamCollection exposedParams{mParams.GenerateUid(), "Exposed Parameters"};
+
+    MaterialGraph(unsigned int _uid);
+
+    void SetupCache();
+
+    void DeleteCache();
+
+    void Process();
+
+private:
+    template <class Archive>
+    void save(Archive &ar, const unsigned int version) const
+    {
+        ar &boost::serialization::base_object<Graph>(*this);
+        ar &size, &width, &height, &imageFormat, &seed;
+        ar &defaultParams, &size, &exposedParams;
+    }
+
+    template <class Archive>
+    void load(Archive &ar, const unsigned int version)
+    {
+        ar &boost::serialization::base_object<Graph>(*this);
+        ar &size, &width, &height, &imageFormat, &seed;
+        ar &defaultParams, &size, &exposedParams;
+    }
+    BOOST_SERIALIZATION_SPLIT_MEMBER()
+};
+BOOST_CLASS_VERSION(MaterialGraph, 1)
 #endif //_MATERIALGRAPH_H
