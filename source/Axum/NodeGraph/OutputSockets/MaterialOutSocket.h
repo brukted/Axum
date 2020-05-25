8eb4230a530334d98d7bba7daee20488666fd533
Bruk Tedla <biruk.tedla93@gmail.com>
2020-05-25 14:00:41 +0300

    made functional

--- source/Axum/NodeGraph/OutputSockets/MaterialOutSocket.h
+++ source/Axum/NodeGraph/OutputSockets/MaterialOutSocket.h
@@ -9,6 +9,10 @@
 #include "../OutputSocket.h"
 #include "../InputSockets/MaterialInSocket.h"
 #include "../Material.h"
+#include <exception>
+#include "Log.h"
+#include <epoxy/gl.h>
+#include "../Nodes/MaterialNode.h"
 
 class MaterialOutSocket : public OutputSocket
 {
@@ -16,10 +20,25 @@ private:
 	/**Id of the texture on gpu used for live preview execution
 	 * exists only if setup cache is called it is 0 if not initalized
 	**/
-	unsigned int GPUtexture = 0;
+	GLuint GTextureId = 0;
+	Channel channel{Channel::Color};
 
 public:
-	Channel channels;
+	MaterialOutSocket(std::string &_name, unsigned int _uid, Channel _channel = Channel::Color);
+
+	MaterialOutSocket(const char *_name, unsigned int _uid, Channel _channel = Channel::Color);
+
+	void SetupCache();
+
+	void DeleteCache();
+
+	GLint GetGlFormat(ImageFormat format);
+	/**
+	 * @brief Return id of the cached texture on the gpu 
+	 *  
+	 * @return GLuint ID of the texture on the GPU
+	 */
+	GLuint GetGPUTexture();
 };
 
 #endif //_MATERIALOUTSOCKET_H
