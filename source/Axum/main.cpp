d86d831f2c2db521e8df10bbd8bc18ffc678e5f9
Bruk Tedla <biruk.tedla93@gmail.com>
2020-07-05 13:52:09 +0300

    add resource loading

--- source/Axum/main.cpp
+++ source/Axum/main.cpp
@@ -1,14 +1,34 @@
 #include "Log.h"
 #include "MApplication.h"
 #include "PathUtils.h"
+#include <gtkmm.h>
 
 using namespace Axum::Utils;
+
+void load_resources();
 
 int main(int argc, char *argv[]) {
   PathUtils::getPaths(argv[0]);
   Log::getInstance().Startup();
+  load_resources();
   AX_LOG_EDITOR_INFO("Application path : {}", PathUtils::excutablePath)
   auto application = Axum::MApplication::create();
   AX_LOG_EDITOR_INFO("Application created.")
   return application->run(argc, argv);
+}
+
+/**
+ * @brief Loads resources embedded or bundled with glib
+ * compile resources tool.
+ *
+ */
+void load_resources() {
+  GError *err = NULL;
+  std::string rPath = PathUtils::resourcesPath + "/resources.bin";
+  GResource *r = g_resource_load(rPath.c_str(), &err);
+  g_resources_register(r);
+  if (err != NULL) {
+    AX_LOG_EDITOR_CRITICAL(err->message)
+    delete err;
+  }
 }
