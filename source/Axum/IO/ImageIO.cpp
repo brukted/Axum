1e252e5fe5c073f500d668cd4d653a956a879a56
Bruk Tedla <biruk.tedla93@gmail.com>
2020-10-25 14:00:28 +0300

    improve imageIO

--- source/Axum/IO/ImageIO.cpp
+++ source/Axum/IO/ImageIO.cpp
@@ -1,39 +1,88 @@
 #include "ImageIO.h"
+#include <OpenImageIO/filesystem.h>
+#include <OpenImageIO/imagebufalgo.h>
+#include <OpenImageIO/imageio.h>
+#include <array>
+#include <assert.h>
+#include <fmt/format.h>
+
+// TODO: Not happy with ther performace of the function
 namespace Axum::IO {
-OIIO::ImageSpec loadImageFromFile(std::string_view filePath,
-                                  std::vector<unsigned char> &pixles) {
+std::tuple<int, int> loadImageFromFile(std::string_view filePath,
+                                       std::vector<unsigned char> &pixles) {
 
   auto input = OIIO::ImageInput::open(filePath.data());
   if (!input)
     throw std::runtime_error(
         fmt::format("Can't load image from {}", filePath.data()));
   auto specs = input->spec();
-  int width = specs.width;
-  int height = specs.height;
-  int channels = specs.nchannels;
-  pixles = std::vector<unsigned char>(width * height * channels);
-  input->read_image(OIIO::TypeDesc::UINT8, &pixles[0]);
-  input->close();
-  return specs;
+  pixles = std::vector<unsigned char>(specs.width * specs.height * 4);
+  if (specs.channelnames == std::vector<std::string>{"R", "G", "B", "A"}) {
+    if (!input->read_image(0, 0, 0, -1, OIIO::TypeDesc::UINT8, pixles.data())) {
+      throw std::runtime_error(
+          fmt::format("Failed loading image from {}", filePath.data()));
+    }
+    input->close();
+    return {specs.full_width, specs.full_height};
+  } else {
+    auto buffer = OIIO::ImageBuf(filePath.data(), 0, 0);
+    int rIdx = buffer.spec().channelindex("R");
+    int gIdx = buffer.spec().channelindex("G");
+    int bIdx = buffer.spec().channelindex("B");
+    int aIdx = buffer.spec().channelindex("A");
+    std::array<int, 4> channelOrder = {rIdx, gIdx, bIdx, aIdx};
+    if (OIIO::ImageBufAlgo::isMonochrome(buffer, buffer.roi_full())) {
+      channelOrder = {rIdx, rIdx, rIdx, aIdx};
+    }
+    OIIO::ImageBufAlgo::channels(buffer, buffer, 4, channelOrder, {1, 1, 1, 1},
+                                 {"R", "G", "B", "A"});
+    if (!buffer.get_pixels(buffer.roi_full(), OIIO::TypeDesc::UINT8,
+                           pixles.data())) {
+      throw std::runtime_error(
+          fmt::format("Failed loading image from {}", filePath.data()));
+    }
+    return {buffer.spec().full_width, buffer.spec().full_height};
+  }
 }
 
-OIIO::ImageSpec loadImageFromMemory(std::string_view fileName, void *data,
-                                    size_t size,
-                                    std::vector<unsigned char> &pixles) {
+std::tuple<int, int> loadImageFromMemory(std::string_view fileName, void *data,
+                                         size_t size,
+                                         std::vector<unsigned char> &pixles) {
   assert(data != nullptr);
   assert(size != 0);
   OIIO::Filesystem::IOMemReader mem_reader(data, size);
   auto input = OIIO::ImageInput::open(fileName.data(), nullptr, &mem_reader);
   if (!input)
     throw std::runtime_error(
-        fmt::format("Can't load embedded image {}", fileName.data()));
+        fmt::format("Can't load image from {}", fileName.data()));
   auto specs = input->spec();
-  int width = specs.width;
-  int height = specs.height;
-  int channels = specs.nchannels;
-  pixles = std::vector<unsigned char>(width * height * channels);
-  input->read_image(OIIO::TypeDesc::UINT8, &pixles[0]);
-  input->close();
-  return specs;
+  pixles = std::vector<unsigned char>(specs.width * specs.height * 4);
+  if (specs.channelnames == std::vector<std::string>{"R", "G", "B", "A"}) {
+    if (!input->read_image(0, 0, 0, -1, OIIO::TypeDesc::UINT8, pixles.data())) {
+      throw std::runtime_error(
+          fmt::format("Failed loading image from {}", fileName.data()));
+    }
+    input->close();
+    return {specs.full_width, specs.full_height};
+  } else {
+    OIIO::ImageBuf buffer =
+        OIIO::ImageBuf(fileName.data(), 0, 0, nullptr, nullptr, &mem_reader);
+    int rIdx = buffer.spec().channelindex("R");
+    int gIdx = buffer.spec().channelindex("G");
+    int bIdx = buffer.spec().channelindex("B");
+    int aIdx = buffer.spec().channelindex("A");
+    std::array<int, 4> channelOrder = {rIdx, gIdx, bIdx, aIdx};
+    if (OIIO::ImageBufAlgo::isMonochrome(buffer, buffer.roi_full())) {
+      channelOrder = {rIdx, rIdx, rIdx, aIdx};
+    }
+    OIIO::ImageBufAlgo::channels(buffer, buffer, 4, channelOrder, {1, 1, 1, 1},
+                                 {"R", "G", "B", "A"});
+    if (!buffer.get_pixels(buffer.roi_full(), OIIO::TypeDesc::UINT8,
+                           pixles.data())) {
+      throw std::runtime_error(
+          fmt::format("Failed loading image from {}", fileName.data()));
+    }
+    return {buffer.spec().full_width, buffer.spec().full_height};
+  }
 }
 } // namespace Axum::IO
--- source/Axum/IO/ImageIO.h
+++ source/Axum/IO/ImageIO.h
@@ -6,21 +6,36 @@
 #ifndef _AXUM_IO_IMAGEIO_H_
 #define _AXUM_IO_IMAGEIO_H_
 
-#include <OpenImageIO/filesystem.h>
-#include <OpenImageIO/imageio.h>
-#include <assert.h>
 #include <string_view>
 #include <tuple>
 #include <vector>
 
 namespace Axum::IO {
 
-OIIO::ImageSpec loadImageFromFile(std::string_view filePath,
-                                  std::vector<unsigned char> &pixles);
+/**
+ * @brief Loads the image at @a filePath to pixles as RGBA 8bit.
+ *
+ * @param filePath path to the image
+ * @param pixles vector to put the pixles
+ * @return std::tuple<int, int> {width,height}
+ * @throw std::runtime_error if it can't load the image
+ */
+std::tuple<int, int> loadImageFromFile(std::string_view filePath,
+                                       std::vector<unsigned char> &pixles);
 
-OIIO::ImageSpec loadImageFromMemory(std::string_view fileName, void *data,
-                                    size_t size,
-                                    std::vector<unsigned char> &pixles);
+/**
+ * @brief Loads the image from memory @a data to pixles as RGBA 8bit.
+ *
+ * @param fileName name of the file before it was loaded to memory.
+ * @param data pointer to the data at memory.
+ * @param size size of the data.
+ * @param pixles vector to put the pixles.
+ * @return std::tuple<int, int> {width,height}
+ * @throw std::runtime_error if it can't load the image
+ */
+std::tuple<int, int> loadImageFromMemory(std::string_view fileName, void *data,
+                                         size_t size,
+                                         std::vector<unsigned char> &pixles);
 
 } // namespace Axum::IO
 #endif // _AXUM_IO_IMAGEIO_H_
